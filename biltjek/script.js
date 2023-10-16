1be8e171857cc858d7fa3861944ad81dccaaa7c2
rosessix <35843808+rosessix@users.noreply.github.com>
2023-10-16 13:26:27 +0200

    bil-tjek.dk push

---
+++ biltjek/script.js
@@ -0,0 +1,90 @@
+const API_URL = 'https://corsproxy.io/?https://www.tjekbil.dk/api/v3/dmr/regnrquery';
+const MOT_URL = 'https://corsproxy.io/?https://www.tjekbil.dk/api/v3/tstyr/reports?vin='
+// Dette kører igennem en corsproxy, da jeg fik en CORS fejl.
+
+const searchPlate = async (plate) => {
+    if (plate == undefined) {
+        plate = $("#numberplate").val();
+    }
+    console.log(plate);
+
+    let request = await fetch(`${API_URL}/${plate}?amount=1`);
+    let json;
+    try {
+        json = await request.json();
+    } catch(err) {
+        return $('#vehicle').html(`<h3>Der kunne ikke findes noget data på dette køretøj.<h3/>`);
+    }
+
+
+    let data = json[0];
+    console.log(data);
+
+    $('#vehicle').html(`
+        <p>Model: ${data.maerkeTypeNavn} ${data.modelTypeNavn} ${data.variantTypeNavn !== undefined ? data.variantTypeNavn : ''}</p>
+        <p>Nummerplade: ${data.regNr}</p>
+        <p>Stel NR: ${data.stelNr}</p>
+        <p>Drivkraft: ${data.drivkraftTypeNavn}</p>
+
+        <div id="vehicle-equipment">Mærkværdigt udstyr: </div>
+    `)
+
+    let dEquip = data.koeretoejUdstyrSamling
+    if (dEquip.length !== 0) {
+        for (const equipment of dEquip) {
+            $('#vehicle-equipment').append(`<p>${equipment}</p>`)
+        }
+    }
+
+    populateMot(data.stelNr);
+    populateExpenses(data)
+}
+
+const populateMot = async (identifier) => {
+    $('#mot').html('Indlæser...')
+    let request = await fetch(`${MOT_URL}${identifier}`)
+    let response = await request.json()
+    let data = response.rapporter;
+    $('#mot').html('')
+    for (const value of data) {
+        let motColor = getMotColor(value.synsresultat)
+        console.log('changing data?')
+        $('#mot').append(`
+        <div class="mot-card ${motColor}">
+            <h3>${value.synsresultat}</h3>
+            <p>${value.firma}</p>
+            ${value.fejl.length !== 0 ? `<p>Fejl: ${value.fejl[0].description}</p>` : ''}
+            <p>Dato: ${value.synsdato}</p>
+            <p>Km stand: ${value.kmstand}</p>
+        </div>
+        `)
+    }
+}
+
+const populateExpenses = async (vehicle) => {
+    $('#expenses').html(`
+        <p>KM/L: ${vehicle.motorKmPerLiter || "Ukendt"}</p>
+
+    `)
+}
+
+const getMotColor = (result) => {
+    switch(result) {
+        case 'Godkendt':
+            return 'green'
+        case 'Betinget godkendt':
+        case 'Middel':
+        case 'Kan godkendes efter omsyn hos (om)synsvirksomhed':
+            return 'yellow'
+        case 'idk':
+            return 'red'
+    }
+
+    return ''
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        searchPlate('DM57724')
+    }, 500);
+})
